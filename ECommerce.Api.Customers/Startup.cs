dfab167a441a20842c9c0dd5eb35d0c9aced994c
RonFudim <98484866+RonFudim@users.noreply.github.com>
2023-12-10 21:02:41 -0500

    Add files via upload

---
+++ ECommerce.Api.Customers/Startup.cs
@@ -0,0 +1,57 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AutoMapper;
+using ECommerce.Api.Customers.Db;
+using ECommerce.Api.Customers.Interfaces;
+using ECommerce.Api.Customers.Providers;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace ECommerce.Api.Customers
+{
+	public class Startup
+	{
+		public Startup(IConfiguration configuration)
+		{
+			Configuration = configuration;
+		}
+
+		public IConfiguration Configuration { get; }
+		
+		public void ConfigureServices(IServiceCollection services)
+		{
+			services.AddDbContext<CustomersDbContext>(options =>
+			{
+				options.UseInMemoryDatabase("Customers");
+			});
+			services.AddScoped<ICustomersProvider, CustomersProvider>();
+			services.AddAutoMapper(typeof(Startup));
+			services.AddControllers();
+		}
+
+		public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+		{
+			if (env.IsDevelopment())
+			{
+				app.UseDeveloperExceptionPage();
+			}
+
+			app.UseRouting();
+
+			app.UseAuthorization();
+
+			app.UseEndpoints(endpoints =>
+			{
+				endpoints.MapControllers();
+			});
+		}
+	}
+}
