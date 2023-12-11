d292afab7a0453f78210e53cade492c2cc678404
RonFudim <98484866+RonFudim@users.noreply.github.com>
2023-12-10 21:00:39 -0500

    Add files via upload

---
+++ ECommerce.Api.Products/Db/Interfaces/IProductsProvider.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using static Microsoft.EntityFrameworkCore.DbLoggerCategory;
+
+namespace ECommerce.Api.Products.Db.Interfaces
+{
+	public interface IProductsProvider
+	{
+		Task<(bool IsSuccess, IEnumerable<Models.Product> Products, string ErrorMessage)> GetProductsAsync();
+		Task<(bool IsSuccess, Models.Product Product, string ErrorMessage)> GetProductAsync(int id);
+	}
+}
